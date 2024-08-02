af76156553c9ff8c3f7e4513216e2b264f088bae
rupeshg27 <rupesh19605@gmail.com>
2024-08-03 00:26:07 +0530

    vite.config.js changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio_7/', // replace 'Portfolio_7' with your repo name
   plugins: [react()],
-})
+});
