c008ed391bf774ca3bbae44820c7fb8daf52bd6f
rupeshg27 <rupesh19605@gmail.com>
2024-08-03 08:55:05 +0530

    CV Upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio_7/', // replace 'Portfolio_7' with your repo name
+  base: '/Portfolio07/', // replace 'Portfolio07' with your repo name
   plugins: [react()],
 });
